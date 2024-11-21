4c429a4569a5c99ce9a6a2ff53a09f60ed9b401a
dani <emotion6172@gmail.com>
2024-11-21 22:57:13 +0900

    241121

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,27 +1,61 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
-import Ex01 from './pages/Ex01';
-import Ex02 from './pages/Ex02';
-import Ex03 from './pages/Ex03';
-import Ex04 from './pages/Ex04';
-import Ex05 from './pages/Ex05';
-import Ex06 from './pages/Ex06';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Ex01 from './pages/Ex01'
+import Ex02 from './pages/Ex02'
+import Ex03 from './pages/Ex03'
+import Ex04 from './pages/Ex04'
+import Ex05 from './pages/Ex05'
+import Ex06 from './pages/Ex06'
 
-
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/1",
+    element: <Ex01 />
+  },
+  {
+    path: "/2",
+    element: <Ex02 />
+  },
+  {
+    path: "/3",
+    element: <Ex03 />
+  },
+  {
+    path: "/4",
+    element: <Ex04 />
+  },
+  {
+    path: "/5",
+    element: <Ex05 />
+  },
+  {
+    path: "/6",
+    element: <Ex06 />
+  }
+], {
+  future: {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+    v7_prependBasename: true
+  },
+  basename: '/' // 필요한 경우 base path 설정
+})
 
 const AppRoutes = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/1" element={<Ex01 />} />
-      <Route path="/2" element={<Ex02 />} />
-      <Route path="/3" element={<Ex03 />} />
-      <Route path="/4" element={<Ex04 />} />
-      <Route path="/5" element={<Ex05 />} />
-      <Route path="/6" element={<Ex06 />} />
-    </Routes>
-  </Router>
-);
+  <RouterProvider 
+    router={router} 
+    future={{
+      v7_startTransition: true
+    }}
+  />
+)
 
-export default AppRoutes;
+export default AppRoutes
